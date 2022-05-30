d2742d5e529190591de4e6b2680dc65e40d4deec
Kristina Rudyte <rkristina32@yahoo.com>
2022-05-30 21:53:11 +0300

    css

--- main/index.php
+++ main/index.php
@@ -5,11 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./Style/main.css">
+    <link rel="stylesheet" href="./images/logo.png">
     <title>Main </title>
 </head>
 
 <body>
     <header class="header">
+        <img href="./bankas/main/images/logo.png">
         <h1>Yo|a bank</h1>
         <nav class="navigation">
             <a href="http://localhost/vienaragiai/bankas/bankas/main/">Home</a>
